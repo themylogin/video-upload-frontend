e0ea6a8b17bb4d953e6668753eac89941028e518
themylogin <themylogin@gmail.com>
2017-04-06 17:47:57 +0300

    Implement logic

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,81 @@
-import angular from 'angular';
+import angular from "angular";
+import "angular-file-upload";
+import "angular-resource";
+import "angular-route";
 
-import '../style/app.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
+class MediaFileController {
+    constructor($scope, mediaFileResource, $routeParams) {
+        this.$scope = $scope;
+        this.mediaFileResource = mediaFileResource;
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
+        this.id = $routeParams.id;
+
+        this.$scope.mediaFile = null;
+        this.$scope.updateMediaFile = this.updateMediaFile.bind(this);
+
+        this.updateMediaFile();
+    }
+
+    updateMediaFile() {
+        this.mediaFileResource.get({ id: this.id }, (mediaFile) => {
+            this.$scope.mediaFile = mediaFile;
+        });
+    }
 }
 
-const MODULE_NAME = 'app';
+class UploadsController {
+    constructor($scope, API_URL, FileUploader, uploadedMediaFileResource) {
+        this.$scope = $scope;
+        this.uploadedMediaFileResource = uploadedMediaFileResource;
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+        $scope.uploader = new FileUploader({
+            url: `${API_URL}/media_files/uploads/upload`,
+            onSuccessItem: this.updateUploadedFilesList.bind(this),
+        });
 
-export default MODULE_NAME;
+        this.$scope.uploadedMediaFiles = [];
+        this.$scope.updateUploadedFilesList = this.updateUploadedFilesList.bind(this);
+        this.updateUploadedFilesList();
+    }
+
+    updateUploadedFilesList() {
+        this.uploadedMediaFileResource.query((uploadedMediaFiles) => {
+            this.$scope.uploadedMediaFiles = uploadedMediaFiles;
+        });
+    }
+}
+
+var app = angular.module("app", [
+    "angularFileUpload",
+    "ngResource",
+    "ngRoute",
+]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            controller: "UploadsController",
+            template: require("./uploads.html"),
+        })
+        .when("/media_files/:id", {
+            controller: "MediaFileController",
+            template: require("./media_file.html"),
+        })
+        ;
+});
+
+app.controller("MediaFileController", ["$scope", "MediaFile", "$routeParams", MediaFileController]);
+app.controller("UploadsController", ["$scope", "API_URL", "FileUploader", "UploadedMediaFile", UploadsController]);
+
+app.factory("MediaFile", function(API_URL, $resource) {
+    return $resource(`${API_URL}/media_files/:id`);
+});
+app.factory("UploadedMediaFile", function(API_URL, $resource) {
+    return $resource(`${API_URL}/media_files/uploads/:id`);
+});
+
+app.value("API_URL", "http://localhost:8000/api/v1");
+
+export default "app";
